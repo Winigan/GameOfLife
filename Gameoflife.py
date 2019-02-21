4d6e4e5e4b1e3a64dac92ef3325d10a08a96a868
Winigan <36704348+Winigan@users.noreply.github.com>
2019-02-21 07:41:56 +0100

    Working Slow UI

--- Gameoflife.py
+++ Gameoflife.py
@@ -69,7 +69,7 @@ def is_empty (board, n, p):
             empty = empty and board[i][j] == 0
     return empty
 
-#This Function print the board 
+#This Function print the hardcoe version of the board 
 def print_board (board, n, p):
     border = "+" + "-"*p + "+"
     print(border)
@@ -78,7 +78,7 @@ def print_board (board, n, p):
         for j in range (p):
             cell = ""
             if board[i][j] == 1:
-                cell = "1"
+                cell = "X"
             else:
                 cell = " "
             line += cell + ""
@@ -90,41 +90,113 @@ def start(n, p):
     Board = create_board(n, p)
 
     #Here you can hardcode a starting state or call existing starts
+    Board[25][60] = 1
+    Board[25][61] = 1
+    Board[25][62] = 1
+    Board[24][61] = 1
+    Board[26][62] = 1
 
     print_board(Board, n, p)
-    #We make the program run as long as the board is not empty (we can look for oscillating paterns in the future)
-    while not is_empty(Board, n, p):
-        #We wait 0.1 ms before itering to the next turn and display it
-        time.sleep(.1)
-        Board = next_turn(Board, n, p)
-        print_board(Board, n, p)
+    return Board
 
+"""
+def run(board, n, p, canvas):
+    #We make the program run as long as the board is not empty (we can look for oscillating paterns in the future)
+    while not is_empty(board, n, p):
+
+        #We wait 0.1 ms before itering to the next turn and display it
+        display_board(board, n, p, canvas)
+        time.sleep(.1)
+        board = next_turn(board, n, p)
+"""
 """
 This section contains all the visual effects such as the creation of the window
 or interactions with the user
 """
 
-#TODO
+#This function get the inputs form the use and starts the simulation
+def get_input(input1, input2):
 
-"""
-Here some code in order to display a window
+    #We get the inputs here and starts the game
+    n = int(input2.get())
+    p = int(input1.get())
 
-window = Tk()
-label = Label(window, text = "Welcome to the Game Of Life")
-label.pack()
-window.mainloop 
+    create_canvas(n,p)
 
-Creating an input field in a window
+def create_canvas(n,p):
+    window = Tk()
+    #Button that closes the window
+    bouton=Button(window, text="Close", command=window.destroy)
+    bouton.pack(side = BOTTOM)
+    
+    height = n*10
+    width = p*10
 
-top = Tk()
-L1 = Label(top, text="User Name")
-L1.pack( side = LEFT)
-E1 = Entry(top, bd =5)
-E1.pack(side = RIGHT)
-top.mainloop()
-"""
+    
+    #We create the canvas that will display our board so we need to create some cells
+    canvas = Canvas(window, width=width, height=height, background="white")
+    for i in range (height):
+        for j in range (width):
+            if i%10 == 0 and j%10 ==0:
+                canvas.create_rectangle(j, i, j + 10, i + 10)
+    canvas.pack()
+
+    #We create a new board and display it on the canvas
+    board = start(n, p)
+    canvas = display_board(board, n, p, canvas)
+    canvas.pack()
+    
+    #We make the program run as long as the board is not empty (we can look for oscillating paterns in the future)
+    while not is_empty(board, n, p):
+        canvas = display_board(board, n, p, canvas)
+        canvas.pack()
+        board = next_turn(board, n, p)
+        window.update()
+
+        
+#The function display the current board on a new window
+def display_board (Board, n, p, canvas):    
+
+    for i in range (n):
+        for j in range (p):
+            color = ""
+            if Board[i][j] == 1:
+                color = "black"
+            else:
+                color = "white"
+            canvas.create_rectangle(10*j, 10*i, 10*j + 10, 10*i + 10, fill=color)
+    return canvas
+
+#This function create a window that could get input from the user
+def create_window():
+
+    #Creation of the window and the head title
+    window = Tk()
+    label = Label(window, text = "Welcome to the Game Of Life")
+    label.pack(side = TOP) 
+
+    #We create two input fields
+    inp1_label = Label(window, text = "Enter the height of your board")
+    inp1_label.pack()
+    input1 = Entry(window, width=30)
+    input1.pack()
+
+    inp2_label = Label(window, text = "Enter the width of your board")
+    inp2_label.pack()
+    input2 = Entry(window, width=30)
+    input2.pack()
+
+    #Button that call the command to get the inputs
+    bouton = Button(window, text="Enter", command=lambda:get_input(input1,input2))
+    bouton.pack()
+
+    #Button that closes the window
+    bouton=Button(window, text="Quit", command=window.quit)
+    bouton.pack(side = BOTTOM)
+
+    window.mainloop()
 
 """ Here you can call any function in order to test if they work """
 
-#Here is the call with a 50 * 120 board
-start(50,120)
+#We call the window
+create_window()
